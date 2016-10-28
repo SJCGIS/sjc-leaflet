56f046c943e8bc2e59d0abc47dd99c9f5a29e50f
Nick Peihl <nickp@sanjuanco.com>
2016-10-28 14:02:54 -0700

    Style location control button states
    This also adds a feature where locations outside the maxBounds of the
    map return an error state.

--- packages/sjc-leaflet-location-control/index.js
+++ packages/sjc-leaflet-location-control/index.js
@@ -4,6 +4,7 @@ require('leaflet-easybutton')
 const sf = require('sheetify')
 
 sf('leaflet-easybutton', { global: true })
+sf('./css/main.css', { global: true })
 
 /**
  * Location control button
@@ -11,34 +12,86 @@ sf('leaflet-easybutton', { global: true })
  * @example
  * var locationControl = sjcLeaflet.locationControl()
  * locationControl.addTo(map)
-*/
+ */
 
-module.exports = () => {
+module.exports = (options) => {
   const locateButton = L.easyButton({
     id: 'location-control-button',
     states: [{
       stateName: 'locate-me',
-      icon: '&target;',
+      icon: 'location location-off',
       title: 'Locate Me',
       onClick: locateUser
+    }, {
+      stateName: 'locating',
+      icon: 'location location-locating location-spin',
+      title: 'Locating',
+      onClick: stopLocate
+    }, {
+      stateName: 'follow-me',
+      icon: 'location location-on',
+      title: 'Following',
+      onClick: stopLocate
+    }, {
+      stateName: 'locate-error',
+      icon: 'location location-error',
+      title: 'Location unavailable',
+      onClick: stopLocate
+    }, {
+      stateName: 'locate-out-bounds',
+      icon: 'location location-error',
+      title: 'Current location out of range of map',
+      onClick: stopLocate
     }]
   })
 
-  locateButton.button.style.fontSize = '22px'
+  function stopLocate (button, map) {
+    button.state('locate-me')
+    map.stopLocate()
+    this.layers.clearLayers()
+    this.map.removeLayer(this.layers)
+  }
 
   function locateUser (button, map) {
+    button.state('locating')
     this.map = map
-    this.map.locate({ setView: true, maxZoom: 16 })
+    this.marker = L.marker([0, 0])
+    this.circle = L.circle([0, 0])
+    this.layers = L.featureGroup([this.marker, this.circle])
+    this.map.locate({ watch: true })
     this.map.on('locationfound', L.bind(onLocationFound, this))
+    this.map.on('locationerror', L.bind(onLocationError, this))
+  }
+
+  function onLocationError (err) {
+    console.log(err.message)
+    this.state('locate-error')
+    this.layers.clearLayers()
+    this.map.removeLayer(this.layers)
   }
 
   function onLocationFound (e) {
-    console.log('onLocationFound', arguments)
-    const rad = e.accuracy / 2
-    L.marker(e.latlng).addTo(this.map)
-    L.circle(e.latlng, {
-      radius: rad
-    }).addTo(this.map)
+    const mapBounds = this.map.options.maxBounds
+    if (!validateLocation(e, mapBounds)) {
+      this.state('locate-out-bounds')
+    } else {
+      this.state('follow-me')
+      const rad = e.accuracy / 2
+      this.marker.setLatLng(e.latlng)
+      this.circle.setLatLng(e.latlng)
+      this.circle.setRadius(rad)
+      this.layers.addTo(this.map)
+      this.map.fitBounds(this.layers.getBounds())
+    }
   }
+
+  function validateLocation (e, bounds) {
+    if (bounds && !bounds.contains(e.latlng)) {
+      return false
+    } else {
+      return true
+    }
+  }
+
   return locateButton
 }
