df2588a22b8a4da344212b108c725b77a9c53974
Nick Peihl <nickp@sanjuanco.com>
2016-12-02 10:17:37 -0800

    Add leaflet sidebar control to default module

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const sjcLeaflet = {
   map: require('./packages/sjc-leaflet-map'),
   basemaps: require('./packages/sjc-leaflet-basemaps'),
   layerControl: require('./packages/sjc-leaflet-layer-control'),
-  locationControl: require('./packages/sjc-leaflet-location-control')
+  locationControl: require('./packages/sjc-leaflet-location-control'),
+  sidebar: require('./packages/sjc-leaflet-sidebar')
 }
 
 module.exports = sjcLeaflet
