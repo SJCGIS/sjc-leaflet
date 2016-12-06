d3fd0fb00131ba3fbae8d13099c15a6e96bd69b5
Nick Peihl <nickp@sanjuanco.com>
2016-12-05 16:36:27 -0800

    Update sjc-leaflet-basemaps example

--- packages/sjc-leaflet-basemaps/example/main.js
+++ packages/sjc-leaflet-basemaps/example/main.js
@@ -1,8 +1,8 @@
 require('leaflet')
-var SjcMap = require('../../sjc-leaflet-map')
+var SjcMap = require('sjc-leaflet-map')
 var baseMaps = require('../')
 
 var base = baseMaps.defaultImageryReference()
-SjcMap('map', {
-  layers: [base]
-})
+var map = SjcMap('map')
+
+base.addTo(map)
